a677e1394854a93af8e9acc7265d0fc48d32e738
강현석 <kekemusa37@gmail.com>
2021-01-07 00:02:21 +0900

    feature/model_layer : [Impl] - ImageResponse

---
+++ KakaoImageSearch/Data/Entity/ImageItem.swift
@@ -0,0 +1,30 @@
+//
+//  ImageItem.swift
+//  KakaoImageSearch
+//
+//  Created by 강현석 on 2021/01/06.
+//
+
+import Foundation
+
+struct ImageItem: Codable {
+    let collection: String
+    let thumbnailUrl: String
+    let imageUrl: String
+    let width: Int
+    let height: Int
+    let displaySitename: String
+    let docUrl: String
+    let datetime: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case collection
+        case thumbnailUrl = "thumbnail_url"
+        case imageUrl = "image_url"
+        case width
+        case height
+        case displaySitename = "display_sitename"
+        case docUrl = "doc_url"
+        case datetime
+    }
+}
---
+++ KakaoImageSearch/Data/Entity/ImageResponse.swift
@@ -0,0 +1,18 @@
+//
+//  ImageResponse.swift
+//  KakaoImageSearch
+//
+//  Created by 강현석 on 2021/01/06.
+//
+
+import Foundation
+
+struct ImageResponse: Codable {
+    let documents: [ImageItem]
+    let meta: Meta
+    
+    private enum CodingKeys: String, CodingKey {
+        case documents
+        case meta
+    }
+}
