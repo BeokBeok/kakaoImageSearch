589232e57861d8aa37eb1b87d158c8889d65663f
강현석 <kekemusa37@gmail.com>
2021-02-01 23:26:28 +0900

    features/model_layer : [Impl] KakaoImageSearchApi

---
+++ KakaoImageSearch/Api/KakaoImageSearchApi.swift
@@ -0,0 +1,26 @@
+//
+//  KakaoApi.swift
+//  KakaoImageSearch
+//
+//  Created by 강현석 on 2021/02/01.
+//
+
+import Alamofire
+
+class KakaoImageSearchApi {
+    private let BASE_URL = "https://dapi.kakao.com"
+    private let DOMAIN = "/v2/search/image"
+    
+    func searchImage(
+        keyword: String,
+        page: Int = 1,
+        size: Int = 10
+    ) -> Alamofire.DataRequest {
+        let apiKey = Bundle.main.infoDictionary!["KAKAO_API_KEY"] as! String
+        return AF.request(
+            BASE_URL + DOMAIN,
+            parameters: ["query" : keyword, "page" : page, "size" : size],
+            headers: ["Authorization" : "KakaoAK \(apiKey)"]
+        )
+    }
+}
