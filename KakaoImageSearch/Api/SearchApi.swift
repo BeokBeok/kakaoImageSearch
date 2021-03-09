9163f666143c92b59771e328e7e0c9e80b9f4342
강현석 <kekemusa37@gmail.com>
2021-03-09 22:44:48 +0900

    [Modify] KakaoImageSearchAPI to SearchAPI

---
+++ KakaoImageSearch/Api/SearchApi.swift
@@ -0,0 +1,33 @@
+//
+//  SearchAPI.swift
+//  KakaoImageSearch
+//
+//  Created by 강현석 on 2021/02/01.
+//
+
+import Alamofire
+
+final class SearchAPI {
+    private static let BASE_URL = "https://dapi.kakao.com"
+    private static let DOMAIN = "/v2/search/image"
+    
+    func search(
+        keyword: String,
+        page: Int = 1,
+        size: Int = 10
+    ) -> Alamofire.DataRequest {
+        let apiKey = Bundle.main.infoDictionary?["KAKAO_API_KEY"] as? String ?? ""
+        return AF
+            .request(
+                SearchAPI.BASE_URL + SearchAPI.DOMAIN,
+                method: .get,
+                parameters: [
+                    "query": keyword.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) ?? "",
+                    "page": page,
+                    "size": size
+                ],
+                headers: ["Authorization" : "KakaoAK \(apiKey)"]
+            )
+            .validate()
+    }
+}
